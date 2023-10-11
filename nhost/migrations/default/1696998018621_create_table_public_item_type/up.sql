e7b82aa744a1ad5d01442219db038f5093d3bbb4
Jared Prather <pratherjared@gmail.com>
2023-10-10 21:09:23 -0500

    feat: Adds nhost config and hasura tables for items and cellars

---
+++ nhost/migrations/default/1696998018621_create_table_public_item_type/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."item_type" ("text" text NOT NULL, PRIMARY KEY ("text") , UNIQUE ("text"));
---
+++ nhost/migrations/default/1696998132412_insert_into_public_item_type/up.sql
@@ -0,0 +1,2 @@
+INSERT INTO "public"."item_type"("text") VALUES (E'WINE');
+INSERT INTO "public"."item_type"("text") VALUES (E'BEER');
