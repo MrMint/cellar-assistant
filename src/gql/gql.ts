a03769e60939f3315a2b31c0bb935a28cf14a545
Jared Prather <pratherjared@gmail.com>
2023-10-12 18:20:05 -0500

    feat: Adds items relationship to cellars

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -13,7 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\n  subscription GetItemsStreamingSubscription {\n    items {\n      id\n      name\n      type\n    }\n  }\n": types.GetItemsStreamingSubscriptionDocument,
+    "\n  subscription GetItemsStream {\n    items {\n      id\n      name\n      type\n    }\n  }\n": types.GetItemsStreamDocument,
 };
 
 /**
@@ -33,7 +33,7 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  subscription GetItemsStreamingSubscription {\n    items {\n      id\n      name\n      type\n    }\n  }\n"): (typeof documents)["\n  subscription GetItemsStreamingSubscription {\n    items {\n      id\n      name\n      type\n    }\n  }\n"];
+export function graphql(source: "\n  subscription GetItemsStream {\n    items {\n      id\n      name\n      type\n    }\n  }\n"): (typeof documents)["\n  subscription GetItemsStream {\n    items {\n      id\n      name\n      type\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
