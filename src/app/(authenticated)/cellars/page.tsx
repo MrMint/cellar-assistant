0e0b92114582f7a0d22e8873b2535bd845d42924
Jared Prather <pratherjared@gmail.com>
2023-10-13 15:52:02 -0500

    feat: Adds create support for cellars

--- src/app/(authenticated)/cellars/page.tsx
+++ src/app/(authenticated)/cellars/page.tsx
@@ -2,7 +2,7 @@
 
 import { graphql } from "@/gql";
 import withAuth from "@/hocs/withAuth";
-import { Avatar, Card, Grid, Link, Typography } from "@mui/joy";
+import { Avatar, Card, CardContent, Grid, Link, Typography } from "@mui/joy";
 import NextLink from "next/link";
 import { useQuery } from "urql";
 
@@ -23,21 +23,28 @@ const Cellars = () => {
   const [{ data }] = useQuery({ query: cellarsQuery });
 
   return (
-    <div>
-      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-        {data?.cellars.map((x) => (
-          <Grid key={x.id} xs={6} md={4}>
-            <Card>
-              <Link overlay component={NextLink} href={`cellars/${x.id}/items`}>
-                <Typography level="title-lg">{x.name}</Typography>
-              </Link>
-              <Typography level="body-sm">{x.createdBy.displayName}</Typography>
-              <Avatar src={x.createdBy.avatarUrl} />
-            </Card>
-          </Grid>
-        ))}
+    <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+      {data?.cellars.map((x) => (
+        <Grid key={x.id} xs={6} md={4}>
+          <Card>
+            <Link overlay component={NextLink} href={`cellars/${x.id}/items`}>
+              <Typography level="title-lg">{x.name}</Typography>
+            </Link>
+            <Typography level="body-sm">{x.createdBy.displayName}</Typography>
+            <Avatar src={x.createdBy.avatarUrl} />
+          </Card>
+        </Grid>
+      ))}
+      <Grid key="add-new-cellar" xs={6} md={4}>
+        <Card>
+          <CardContent>
+            <Link overlay component={NextLink} href={`cellars/add`}>
+              <Typography level="title-md">Add a new cellar</Typography>
+            </Link>
+          </CardContent>
+        </Card>
       </Grid>
-    </div>
+    </Grid>
   );
 };
 
