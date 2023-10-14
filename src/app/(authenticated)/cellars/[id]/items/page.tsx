0dab008757a91748f6ddaf1cb94951e38bf91c23
Jared Prather <pratherjared@gmail.com>
2023-10-14 15:37:14 -0500

    feat: Updates item cards

--- src/app/(authenticated)/cellars/[id]/items/page.tsx
+++ src/app/(authenticated)/cellars/[id]/items/page.tsx
@@ -2,8 +2,51 @@
 
 import { graphql } from "@/gql";
 import withAuth from "@/hocs/withAuth";
-import { Box, Card, Grid, Typography } from "@mui/joy";
-import { useQuery, useSubscription } from "urql";
+import {
+  AspectRatio,
+  Box,
+  Card,
+  CardActions,
+  CardOverflow,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/joy";
+import Image from "next/image";
+import { useQuery } from "urql";
+import beer1 from "@/app/public/beer1.png";
+import { MdFavoriteBorder } from "react-icons/md";
+
+type ItemCardProps = {
+  item: {
+    id: string;
+    name: string;
+  };
+  type: "BEER" | "WINE";
+};
+
+const ItemCard = ({ item, type }: ItemCardProps) => (
+  <Card>
+    <CardOverflow>
+      <AspectRatio ratio="2">
+        {type === "BEER" && (
+          <Image src={beer1} alt="An image of a beer glass" fill />
+        )}
+      </AspectRatio>
+    </CardOverflow>
+    <Typography level="title-lg">{item.name}</Typography>
+    <CardActions buttonFlex="0 1 120px">
+      <IconButton
+        variant="outlined"
+        color="neutral"
+        sx={{ mr: "auto" }}
+        disabled
+      >
+        <MdFavoriteBorder />
+      </IconButton>
+    </CardActions>
+  </Card>
+);
 
 const itemsSub = graphql(`
   query GetItemsQuery($cellarId: uuid!) {
@@ -28,11 +71,8 @@ const Items = ({ params: { id } }: { params: { id: string } }) => {
     <Box sx={{ height: "100%", width: "100%" }}>
       <Grid container spacing={2} sx={{ flexGrow: 1 }}>
         {res?.data?.beers.map((x) => (
-          <Grid key={x.id} xs={6} md={4}>
-            <Card>
-              <Typography level="title-lg">{x.name}</Typography>
-              <Typography level="body-sm">April 24 to May 02, 2021</Typography>
-            </Card>
+          <Grid key={x.id} xs={12} sm={6} md={4}>
+            <ItemCard item={x} type="BEER" />
           </Grid>
         ))}
         {res?.data?.items.map((x) => (
