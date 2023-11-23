a6ca20621b00f0f271f9d02c7a62e2b58ddeb9b1
Jared Prather <pratherjared@gmail.com>
2023-11-23 14:00:10 -0600

    feat: Adds scroll restoration to cellar items page when returning from item

--- src/utilities/hooks.ts
+++ src/utilities/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useInterval(callback: () => void, delay: number) {
   const intervalRef = useRef<any>(null);
@@ -13,3 +13,24 @@ export function useInterval(callback: () => void, delay: number) {
   }, [delay]);
   return intervalRef;
 }
+
+const getHash = () =>
+  typeof window !== "undefined"
+    ? decodeURIComponent(window.location.hash.replace("#", ""))
+    : undefined;
+
+export const useHash = () => {
+  const [hash, setHash] = useState(getHash());
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setHash(getHash());
+    };
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return hash;
+};
